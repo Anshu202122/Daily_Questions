3b3525b973590a7ac251b29bec16fa1f379d06e5
Anshu <anshupriya202122@gmail.com>
2025-01-21 21:41:59 +0530

    GFG1

---
+++ GFG/21_01_25_LinkedListGroupReverse.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    Node *reverseKGroup(Node *head, int k) {
+        int n=1;
+        Node* prev=NULL;
+        Node* curr=head;
+        Node* nextNode=NULL;
+        while(curr && n<=k){
+            nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
+            n++;
+        }
+        if(nextNode){
+            head->next=reverseKGroup(nextNode, k);
+        }
+        return prev;
+    }
+};
