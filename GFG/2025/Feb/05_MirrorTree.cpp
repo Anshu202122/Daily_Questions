e42c95a5d5dcd8f3a8f6b6b9ea02c8a1353f11a5
Anshu <anshupriya202122@gmail.com>
2025-02-05 17:08:30 +0530

    GFG POTD

---
+++ GFG/2025/Feb/05_MirrorTree.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    void mirror(Node *node)
+    {
+        if (!node)
+        {
+            return;
+        }
+        swap(node->left, node->right);
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
