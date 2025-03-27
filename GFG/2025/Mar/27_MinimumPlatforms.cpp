ad2953ca8e9fdc535a702fb21449fd81e7caef25
Anshu <anshupriya202122@gmail.com>
2025-03-27 21:25:35 +0530

    GFG POTD

---
+++ GFG/2025/Mar/27_MinimumPlatforms.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int findPlatform(vector<int> &arr, vector<int> &dep)
+    {
+        int n = arr.size(), count = 0, ans = 1, i = 0, j = 0;
+        sort(arr.begin(), arr.end());
+        sort(dep.begin(), dep.end());
+        while (i < n && j < n)
+        {
+            if (arr[i] <= dep[j])
+            {
+                count++;
+                i++;
+            }
+            else
+            {
+                count--;
+                j++;
+            }
+            ans = max(ans, count);
+        }
+        ans = max(ans, count);
+        return ans;
+    }
+};
