f6785864a677e9af2cf107ece3d441df62abb77f
Anshu <anshupriya202122@gmail.com>
2025-04-03 23:25:33 +0530

    GFG POTD

--- GFG/2025/Apr/03_RottenOranges.cpp
+++ GFG/2025/Apr/03_RottenOranges.cpp
@@ -23,8 +23,7 @@ public:
                 }
             }
         }
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
+        int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
         while (!q.empty())
         {
             int k = q.size();
