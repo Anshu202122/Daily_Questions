7d97d4211d25dfdd75965370810bad9f239c58ce
Anshu <anshupriya202122@gmail.com>
2025-05-09 21:33:44 +0530

    GFG POTD

---
+++ GFG/2025/May/09_LargestNumberInK-Swaps.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    string findMaximumNum(string &s, int k)
+    {
+        if (k == 0)
+        {
+            return s;
+        }
+        int n = s.length();
+        string ans = s;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (s[i] < s[j])
+                {
+                    swap(s[i], s[j]);
+                    ans = max(ans, findMaximumNum(s, k - 1));
+                    swap(s[i], s[j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
