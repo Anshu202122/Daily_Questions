6c1400d2f6fa48240f9dacc5d3f084e853642ce6
Anshu <anshupriya202122@gmail.com>
2025-06-16 09:22:44 +0530

    Leetcode POTD

---
+++ Leetcode/2025/Jun/16_MaximumDifferenceBetweenIncreasingElements.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int maximumDifference(vector<int> &nums)
+    {
+        int n = nums.size(), ans = -1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[j] == nums[i])
+                {
+                    continue;
+                }
+                ans = max(ans, nums[j] - nums[i]);
+            }
+        }
+        return ans;
+    }
+};
