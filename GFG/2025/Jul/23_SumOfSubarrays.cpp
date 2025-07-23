c7aa978ffca51ca9e2658de4fe839c3d00a3c4eb
Anshu <anshupriya202122@gmail.com>
2025-07-23 20:02:16 +0530

    GFG POTD

---
+++ GFG/2025/Jul/23_SumOfSubarrays.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int subarraySum(vector<int> &arr)
+    {
+        int n = arr.size(), ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans += (arr[i] * (i + 1) * (n - i));
+        }
+        return ans;
+    }
+};
