e9f1c5220a6e963577a83854727975f00cf6a13b
Anshu <anshupriya202122@gmail.com>
2025-07-30 20:32:07 +0530

    GFG POTD

---
+++ GFG/2025/Jul/30_SubarraysWithSumK.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int cntSubarrays(vector<int> &arr, int k)
+    {
+        int n = arr.size(), sum = 0, ans = 0;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+            if (sum == k)
+            {
+                ans++;
+            }
+            if (mp.find(sum - k) != mp.end())
+            {
+                ans += mp[sum - k];
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
