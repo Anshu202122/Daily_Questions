1c17e90a53a7bb478db5462044c48ee4731b27a3
Anshu <anshupriya202122@gmail.com>
2025-08-05 15:24:57 +0530

    Leetcode POTD

---
+++ Leetcode/2025/Aug/05_FruitIntoBaskets-II.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int numOfUnplacedFruits(vector<int> &fruits, vector<int> &baskets)
+    {
+        int n = fruits.size(), count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (baskets[j] >= fruits[i])
+                {
+                    count++;
+                    baskets[j] = -1;
+                    break;
+                }
+            }
+        }
+        return n - count;
+    }
+};
